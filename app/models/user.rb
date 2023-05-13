8f0b91b24e1bc66e7b5e7078cd3e462eef43057f
ikutanaoko <132249442+ikutanaoko@users.noreply.github.com>
2023-05-13 07:56:14 +0000

    05131656-favorite

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :books
+  has_many :favorites, dependent: :destroy
   has_one_attached :profile_image
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
@@ -18,4 +19,6 @@ class User < ApplicationRecord
     end
     profile_image
   end
+  
+
 end
