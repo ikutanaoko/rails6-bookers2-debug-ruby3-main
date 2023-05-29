f86ec33975e5eafb7234b59b85869185172baad0
ikutanaoko <132249442+ikutanaoko@users.noreply.github.com>
2023-05-29 12:04:56 +0000

    05292104

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,12 +21,12 @@ class User < ApplicationRecord
   validates :introduction, length: { maximum: 50 }
 
 
-  def get_profile_image
+  def get_profile_image(width, heigh)
     unless profile_image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
       profile_image.attach(io: File.open(file_path), filename: 'default-image-jpg', content_type: 'image/jpeg')
     end
-    profile_image
+    profile_image.variant(resize_to_limit: [width, heigh]).processed
   end
 
   def follow(user_id)
