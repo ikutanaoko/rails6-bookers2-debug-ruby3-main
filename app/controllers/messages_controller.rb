c8e4d1b9248ff20b98f19298dca88732f73d6bea
ikutanaoko <132249442+ikutanaoko@users.noreply.github.com>
2023-06-02 12:07:53 +0000

    06022107

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,8 +3,7 @@ class MessagesController < ApplicationController
   def create
     if Entry.where(user_id: current_user.id, room_id: params[:message][:room_id]).present?
       @message = Message.create(params.require(:message).permit(:user_id, :comment, :room_id).merge(user_id: current_user.id))
-    else
-      flash[:alert] = "送信失敗"
+
     end
     redirect_to room_path(@message.room_id)
   end
