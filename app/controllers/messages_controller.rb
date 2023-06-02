910e849f4f85e736e68acde833fd4c49b862e9ac
ikutanaoko <132249442+ikutanaoko@users.noreply.github.com>
2023-06-02 13:43:10 +0000

    06022243

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,10 +1,10 @@
 class MessagesController < ApplicationController
   
   def create
+      @room = Room.find(params[:message][:room_id])
+      @messages = @room.messages
     if Entry.where(user_id: current_user.id, room_id: params[:message][:room_id]).present?
       @message = Message.create(params.require(:message).permit(:user_id, :comment, :room_id).merge(user_id: current_user.id))
-
     end
-    redirect_to room_path(@message.room_id)
   end
 end
