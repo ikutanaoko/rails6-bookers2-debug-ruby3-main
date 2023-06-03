d47baa55123eb2a834fba4af9d30e10f581d2269
ikutanaoko <132249442+ikutanaoko@users.noreply.github.com>
2023-06-03 15:40:16 +0000

    [start]ApplicationTask9c-06040040

---
+++ app/mailers/group_event_mailer.rb
@@ -0,0 +1,2 @@
+class GroupEventMailer < ApplicationMailer
+end
---
+++ test/mailers/group_event_mailer_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class GroupEventMailerTest < ActionMailer::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
