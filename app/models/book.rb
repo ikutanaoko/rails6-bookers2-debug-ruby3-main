2c329a43cc5f5767624772f0c85926f36132fcbd
ikutanaoko <132249442+ikutanaoko@users.noreply.github.com>
2023-06-05 06:30:54 +0000

    [finish]ApplicationTask7b-06051530

--- app/models/book.rb
+++ app/models/book.rb
@@ -59,7 +59,10 @@ class Book < ApplicationRecord
     
   end
   
-  
+  scope :created_today, -> { where(created_at: Time.zone.now.all_day) }
+  scope :created_days_ago, ->(n) { where(created_at: n.days.ago.all_day) }
+  scope :created_this_week, -> {where(created_at: 6.day.ago.beginning_of_day..Time.zone.now.end_of_day) }
+  scope :created_last_week, -> {where(created_at: 2.week.ago.beginning_of_day..1.week.ago.end_of_day) }
   # def self.search(keyword)
   #   Book.where('category LIKE?', "#{keyword}")
   # end
