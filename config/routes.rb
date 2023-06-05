b699540b058e4a5b8dbcec024255ba3cc1ede924
ikutanaoko <132249442+ikutanaoko@users.noreply.github.com>
2023-06-05 11:16:15 +0000

    [start]ApplicationTask9b-06052016

--- config/routes.rb
+++ config/routes.rb
@@ -20,9 +20,9 @@ Rails.application.routes.draw do
   
   resources :users, only: [:index,:show,:edit,:update] do
     resource :relationships, only: [:create, :destroy]
-    get 'followings' => 'relationships#followings', as: 'followings'
-    get 'followers' => 'relationships#followers', as: 'followers'
-    
+    get "followings" => 'relationships#followings', as: 'followings'
+    get "followers" => 'relationships#followers', as: 'followers'
+    get "search" => "users#search"
   end
   
   resources :messages,only: [:create]
